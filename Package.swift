7fc3eef57f7f7ce98f943f01469b5cdf5f192e1e
C. Heath <hexleytheplatypus@icloud.com>
2024-03-14 07:45:04 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,21 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2024 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
 
 import PackageDescription
 
 let package = Package(
     name: "ArbitraryPrecisionIntegers",
-    platforms: [
+    platforms: [ /// For `StaticBigInt`
         .macOS(.v14),
         .iOS(.v17),
         .tvOS(.v17),
